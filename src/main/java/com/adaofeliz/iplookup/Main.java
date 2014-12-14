208019ac8d5fc325bb33ea19f78027024945341c
adaofeliz <pinto@clipkit.de>
2014-12-14 19:51:02 +0000

    Disabling spring-boot-starter-actuator; Adding ComponentScan annotation

--- src/main/java/com/adaofeliz/iplookup/Main.java
+++ src/main/java/com/adaofeliz/iplookup/Main.java
@@ -2,6 +2,7 @@ package com.adaofeliz.iplookup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 
 @EnableWebMvc
+@ComponentScan
 @EnableAutoConfiguration
 public class Main {
 
